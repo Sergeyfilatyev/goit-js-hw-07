7a7956eb57337e5081ab7a9ba47b73a880577e35
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-09-16 09:06:35 +0300

    img-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(({ description, original, preview }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+galleryBox.insertAdjacentHTML('afterbegin', markup);
+galleryBox.addEventListener('click', openModal);
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.classList.value !== 'gallery__image') {
+    return;
+  }
+
+  const imgSrc = event.target.dataset.source;
+  //   console.log(imgSrc);
+  const instance = basicLightbox.create(`
+    <img src="${imgSrc}" width="800" height="600">
+    `);
+
+  instance.show();
+  window.addEventListener('keydown', closeModalEsc);
+}
+function closeModalEsc(event) {
+  const modalImg = document.querySelector('.basicLightbox');
+  if (event.key !== 'Escape') {
+    return;
+  }
+
+  modalImg.remove();
+  window.removeEventListener('keydown', closeModalEsc);
+  console.log(event.key);
+}
